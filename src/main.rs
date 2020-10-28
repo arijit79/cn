43f75269a157bf5a70154d5335dfd6e382d2ef3d
Arijit Dey <arijid79@gmail.com>
2020-10-28 18:19:02 +0530

    Do a bunch of code improvements
    Changed the stream of errors to stderr and format the code

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,27 @@ use std::path::Path;
 
 fn main() {
     let cli = App::new("cn")
-                .version("0.1.0")
-                .author("Arijit Dey <arijid79@gmail.com>")
-                .about("Copy SOURCE to DESTINATION")
-                .arg(Arg::with_name("source").takes_value(true)
-                    .value_name("SOURCE")
-                    .help("The paths that needs to be copied")
-                    .required(true))
-                .arg(Arg::with_name("dest").takes_value(true)
-                    .value_name("DESTINATION")
-                    .help("The directory where the files need to be placed")
-                    .required(true)
-            )
-                .get_matches();
+        .version("0.1.0")
+        .author("Arijit Dey <arijid79@gmail.com>")
+        .about("Copy SOURCE to DESTINATION")
+        .arg(
+            Arg::with_name("source")
+                .takes_value(true)
+                .value_name("SOURCE")
+                .help("The paths that needs to be copied")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("dest")
+                .takes_value(true)
+                .value_name("DESTINATION")
+                .help("The directory where the files need to be placed")
+                .required(true),
+        )
+        .get_matches();
 
-    let source = Path::new(cli.value_of("source").unwrap())
-            .to_owned();
-    let mut dest = Path::new(cli.value_of("dest").unwrap())
-            .to_owned();
+    let source = Path::new(cli.value_of("source").unwrap()).to_owned();
+    let mut dest = Path::new(cli.value_of("dest").unwrap()).to_owned();
 
     if Path::new(&dest).is_dir() {
         dest.push(&source.file_name().unwrap().to_str().unwrap());
@@ -31,6 +34,6 @@ fn main() {
 
     match r {
         Ok(_) => println!("{} -> {}", source.display(), dest.display()),
-        Err(e) => println!("Copying aborted...\n\n{}", e)
+        Err(e) => eprintln!("Copying aborted...\n\n{}", e),
     }
 }
